212182537b379232a2622528544bba5f379d2bde
MuhammedFahizk <fahizk100@gmail.com>
2024-11-26 23:36:26 +0530

    version 0.1.5

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,12 @@ import svgToDataUri from "mini-svg-data-uri";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{ts,tsx}",
+      "./components/**/*.{ts,tsx}",
+      "./app/**/*.{ts,tsx}",
+      "./src/**/*.{ts,tsx}",
+      "./data/**/*.{ts,tsx}",
+   
   ],
   theme: {
     extend: {
